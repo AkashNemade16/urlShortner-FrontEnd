86eaa69e1701f2793803eb7a762f9f2e199eeedd
Akash Nemade <aksh.akash@gmail.com>
2021-09-25 20:16:02 -0700

    redirects

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 
 const API = axios.create({ baseURL: 'https://tinyurl-mern.herokuapp.com' })
-
 export const fetchUrl = () => API.get('/get');
 export const createUrl = (newUrl) => API.post('/create', newUrl);
 export const fetchUserUrls = (email) => API.post('/users', email)
