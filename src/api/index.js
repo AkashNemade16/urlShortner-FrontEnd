2cf27cb1ec00108ed8d95cbc162aa61950583f87
Akash Nemade <aksh.akash@gmail.com>
2021-09-25 22:17:04 -0700

    backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'https://tinyurl-mern.herokuapp.com' })
+const API = axios.create({ baseURL: "http://localhost:5000" })
 export const fetchUrl = () => API.get('/get');
 export const createUrl = (newUrl) => API.post('/create', newUrl);
 export const fetchUserUrls = (email) => API.post('/users', email)
